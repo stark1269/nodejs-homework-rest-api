eb26a25755b69594f547b03d16405a5e5e8ea39d
stark1269 <stark1269@gmail.com>
2023-07-21 16:59:30 +0300

    adds file handling and avatar update route

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const ctrl = require('../../controllers/users');
 
 const auth = require('../../middlewares/auth');
+const upload = require('../../middlewares/upload');
 const validateBody = require('../../middlewares/validateBody');
 const { joiSchemaRegisterAndLogin, joiSchemaSubscription } = require('../../schema/JoiSchema');
 
@@ -11,6 +12,7 @@ router.post('/register', validateBody(joiSchemaRegisterAndLogin), ctrl.register)
 router.post('/login', validateBody(joiSchemaRegisterAndLogin), ctrl.login);
 router.post('/logout', auth, ctrl.logout);
 router.get('/current', auth, ctrl.current);
+router.patch('/avatars', auth, upload.single('avatar'), ctrl.updateAvatar);
 router.patch('/:contactId', auth, validateBody(joiSchemaSubscription), ctrl.updateUser);
 
 module.exports = router;
